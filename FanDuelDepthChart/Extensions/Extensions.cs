291e3213a47ea3ce7ace19bbda7a5c14a0a8a30a
rohithakim <rohithakim@gmail.com>
2022-07-11 16:10:13 +1000

    Add files via upload

---
+++ FanDuelDepthChart/Extensions/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using FanDuelDepthChart.Enums;
+
+namespace FanDuelDepthChart.Extensions  
+{
+    public static class EnumExtensions
+    {
+        public static string GetStringValue(this Enum value)
+        {
+            // Get the type
+            Type type = value.GetType();
+
+            // Get fieldinfo for this type
+            FieldInfo fieldInfo = type.GetField(value.ToString());
+
+            // Get the stringvalue attributes
+            StringValueAttribute[] attribs = fieldInfo.GetCustomAttributes(
+                typeof(StringValueAttribute), false) as StringValueAttribute[];
+
+            // Return the first if there was a match.
+            return attribs.Length > 0 ? attribs[0].StringValue : null;
+        }
+    }
+}
